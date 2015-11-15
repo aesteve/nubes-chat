50a039eab996ef804b97019325a5ba7b89a01989
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-11-15 13:55:27 +0100

    missing pagination context

--- src/main/java/com/github/aesteve/nubes/examples/chat/controllers/api/ChatRooms.java
+++ src/main/java/com/github/aesteve/nubes/examples/chat/controllers/api/ChatRooms.java
@@ -14,6 +14,7 @@ import com.github.aesteve.vertx.nubes.annotations.params.RequestBody;
 import com.github.aesteve.vertx.nubes.annotations.routing.http.GET;
 import com.github.aesteve.vertx.nubes.annotations.routing.http.POST;
 import com.github.aesteve.vertx.nubes.annotations.services.Service;
+import com.github.aesteve.vertx.nubes.context.PaginationContext;
 
 @Controller("/api/rooms/")
 @ContentType("application/json")
@@ -24,7 +25,7 @@ public class ChatRooms {
 	
 	@GET
 	@RetrieveByQuery
-	public FindBy<ChatRoom> list() {
+	public FindBy<ChatRoom> list(PaginationContext context) {
 		return new FindBy<>(ChatRoom.class);
 	}
 	
